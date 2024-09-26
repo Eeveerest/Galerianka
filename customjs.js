d2e836d75df8a39d37af7611c673c170814134a2
Eeveerest <moeve5699@gmail.com>
2024-09-26 22:13:21 +0200

    Add files via upload

---
+++ customjs.js
@@ -0,0 +1,19 @@
+let items = document.querySelectorAll('.carousel1 .item1')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+$("#heart").click(function() {
+  $(this).toggleClass("fa-heart fa-heart-o");
+});
