8bc23d46abe02b9d963b8406b40047af41f20b35
Eeveerest <moeve5699@gmail.com>
2024-10-02 00:21:49 +0200

    Update database.php
    Changed connection to online site

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'galerianka';
-$user = 'root';
-$pass = '';
+$host = 'shopianka.ct8.pl';
+$dbname = 'm49159_galerianka';
+$user = 'm49159_admin';
+$pass = 'hygCH2AXdVMP8Fa';
 
 try {
     $pdo = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $pass);
