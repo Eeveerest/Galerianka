be43c669036f6c1e427a2e93bb5297b303f8c578
Eeveerest <moeve5699@gmail.com>
2024-10-03 21:31:28 +0200

    Update RegisterJS.js
    Fixed registration javascript

--- RegisterJS.js
+++ RegisterJS.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){    
-    Register();
-
+    Login();
+    Change();
+    Out();
 });
 var admin_log;
 
-function Register(){
-    $('#formRegister').on('submit',function(e) {
+function Login(){
+     $('#formRegister').on('submit',function(e) {
         e.preventDefault();
             $.ajax({
                url: "RegisterValid.php",
@@ -19,7 +20,7 @@ function Register(){
                       Postcode: $('#Postcode').val(),   
                       HouseNumber: $('#HouseNumber').val(),   
                       Password: $('#Password').val(),   
-                      RepPassword: $('#RepPassword').val(),   
+                      RepPassword: $('#RepPassword').val()   
                 },
                 success: function (response)
                     {
@@ -31,48 +32,48 @@ function Register(){
                         }
                         else{
                               if (response.match("1")){
-                               $('#UserError').html('Nie poprawny login.');
+                               $('#UserError').html('Niepoprawny login.');
                               }else{
                                $('#UserError').html('');
                               }
                             
                               if (response.match("2")){
-                               $('#EmailError').html('Nie poprawny Email.');
+                               $('#EmailError').html('Niepoprawny Email.');
                               }else{
                                $('#EmailError').html('');
                                    }
                             
                               if (response.match("3")){
-                               $('#FirstNameError').html('Nie poprawne imie.');
+                               $('#FirstNameError').html('Niepoprawne imie.');
                               }else{
                                $('#FirstNameError').html('');
                                    }
                             if (response.match("4")){
-                               $('#LastNameError').html('Nie poprawne nazwisko.');
+                               $('#LastNameError').html('Niepoprawne nazwisko.');
                               }else{
                                $('#LastNameError').html('');
                                    }
                             
                               if (response.match("5")){
-                               $('#CityError').html('Nie poprawne miasto.');
+                               $('#CityError').html('Niepoprawne miasto.');
                               }else{
                                $('#CityError').html('');
                                    }
                             
                               if (response.match("6")){
-                               $('#PostcodeError').html('Nie poprawny kod pocztowy');
+                               $('#PostcodeError').html('Niepoprawny kod pocztowy');
                               }else{
                                $('#PostcodeError').html('');
                                    }
                             
                               if (response.match("7")){
-                               $('#HouseNumberError').html('Nie poprawny numer domu.);
+                               $('#HouseNumberError').html('Niepoprawny numer domu.');
                               }else{
                                $('#HouseNumberError').html('');
                                    }
                             
                               if (response.match("8")){
-                               $('#PasswordError').html('Nie poprawne hasło D:');
+                               $('#PasswordError').html('Niepoprawne hasło D:');
                               }else{
                                $('#PasswordError').html('');
                                    }
@@ -82,6 +83,11 @@ function Register(){
                               }else{
                                $('#RepPasswordError').html('');
                               }
+                          if (response.match("10")){
+                               $('#RulesError').html('Niezaznaczone pole.');
+                              }else{
+                               $('#RulesError').html('');
+                              }
                            }
                         
                     }
@@ -90,3 +96,130 @@ function Register(){
       }
 
 
+function account(){
+    var acc = document.getElementById("account");
+    var roll = document.getElementById("logedin");
+    var button = document.getElementById("acc_books");
+    var res = document.getElementById("res_books");
+    var out = document.getElementById("log_out");
+    roll.style.display = "none";
+    acc.style.display = "block";
+    button.style.display = "block";
+    res.style.display = "block";
+    out.style.display = "block";
+
+}      
+
+function admin(){
+    var bookton = document.getElementById("res_books_u");
+    bookton.style.display = "block";
+        $('#acc_books_ref').text('Rezerwacje i wyporzyczenia');
+        $('#res_books_ref').text('Urzytkownicy');
+        $('#book_books_ref').text('Książki');
+        document.getElementById("acc_books_ref").href='admin_pan.php';
+        document.getElementById("res_books_ref").href='users_pan.php';
+        document.getElementById("book_books_ref").href='books_pan.php';
+}
+
+function Change(){
+    $('#accountform').on('submit',function(e) {
+      e.preventDefault();
+          $.ajax({
+             url: "changeData.php",
+              method: 'POST',
+              data: {
+                InputName: $('#named').val(),
+                InputSur: $('#surname').val(),
+                InputAdres: $('#haddress').val(),
+                InputNumber: $('#doc').val(),  
+                InputPass: $('#password').val(),              
+              },
+              success: function (response)
+                  {
+                      
+                      if (response=="Zapisano")
+                      {
+                            var acc = document.getElementById("account");
+                            var button = document.getElementById("acc_books");
+                            var res = document.getElementById("res_books");
+                            var roll = document.getElementById("logedin");
+                            button.style.display = "none";
+                            res.style.display = "none";
+                            acc.style.display = "none";
+                            roll.style.display = "block";
+                            setTimeout(account, 1500);
+                            $('#nameError').html('');
+                            $('#surError').html('');
+                            $('#adrError').html('');
+                            $('#numberError').html('');
+                            $('#passError').html('');   
+                      }
+                      else{
+                        if (response.match("1")){
+                         $('#nameError').html('Wymagane imie');
+                        }else{
+                         $('#nameError').html('');
+                        }
+                        if (response.match("2")){
+                         $('#surError').html('Wymagane nazwisko');
+                        }else{
+                         $('#surError').html('');
+                        }
+                        if (response.match("3")){
+                         $('#adrError').html('Wymagany adres');
+                        }else{
+                         $('#adrError').html('');
+                        }
+                        if (response.match("4")){
+                         $('#numberError').html('Wymagany numer dokumentu');
+                        }else{
+                         $('#numberError').html('');
+                        }
+                        if (response.match("5")){
+                         $('#passError').html('Wymagane hasło');
+                        }else{
+                         $('#passError').html('');
+                        }
+                     }
+                      
+                  },
+              error: function ()
+              {
+                  alert("Wystąpił błąd");
+              }
+          });
+     });
+    };
+
+function Out(){
+    $('#log_out_ref').on('click',function(e) {
+        e.preventDefault();
+            $.ajax({
+                url: 'log_out.php',
+                success: function(){
+                    window.location.href = "/account.php";
+                }
+            });
+           
+    })
+}
+
+function prev_loged(){
+    var log = document.getElementById("login");
+    var roll = document.getElementById("logedin");
+                                                                              
+    log.style.display = "none";
+    roll.style.display = "block";
+    setTimeout(account, 1500);                       
+    if (admin_log==1){
+        setTimeout(admin, 1500);
+        
+    }
+    else{
+        let ref = "books.php?id="+ID;
+        let ref2 = "reserve.php?id="+ID;
+        document.getElementById("acc_books_ref").href=ref;
+        document.getElementById("res_books_ref").href=ref2;
+    }
+
+}
