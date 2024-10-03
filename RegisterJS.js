3c441f72ba656b5096ee8c7302aa86980756b5a8
86-guy <86guy.private@gmail.com>
2024-10-03 14:33:45 +0200

    Update RegisterJS.js
    Create register ajax base

--- RegisterJS.js
+++ RegisterJS.js
@@ -1,1 +1,43 @@
+$(document).ready(function(){    
+    Register();
+
+});
+var admin_log;
+
+function Register(){
+    $('#loginform').on('submit',function(e) {
+        e.preventDefault();
+            $.ajax({
+               url: "LoginValid.php",
+                method: 'POST',
+                data: {
+                      Username: $('#Username').val(),
+                      Password: $('#Password').val()            
+                },
+                success: function (response)
+                    {
+                        
+                        if (response.match("Zalogowano"))
+                        {
+                          window.location.href = "/UserPanel.php";
+
+                        }
+                        else{
+                              if (response.match("username")){
+                               $('#UserError').html('Nie poprawny login ):');
+                              }else{
+                               $('#UserError').html('');
+                              }
+                              if (response.match("password")){
+                               $('#passwordError').html('Nie poprawne hasło D:');
+                              }else{
+                               $('#passwordError').html('');
+                              }
+                           }
+                        
+                    }
+            });  
+       });
+      }
+
 
