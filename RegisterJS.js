714042ee439a5491e70bc3d3ced1f659283491d2
86-guy <86guy.private@gmail.com>
2024-10-03 14:52:01 +0200

    Update RegisterJS.js
    Added error responses and updated ajax to handle register form

--- RegisterJS.js
+++ RegisterJS.js
@@ -5,33 +5,82 @@ $(document).ready(function(){
 var admin_log;
 
 function Register(){
-    $('#loginform').on('submit',function(e) {
+    $('#formRegister').on('submit',function(e) {
         e.preventDefault();
             $.ajax({
-               url: "LoginValid.php",
+               url: "RegisterValid.php",
                 method: 'POST',
                 data: {
                       Username: $('#Username').val(),
-                      Password: $('#Password').val()            
+                      Email: $('#Email').val(),     
+                      FirstName: $('#FirstName').val(),   
+                      LastName: $('#LastName').val(),   
+                      City: $('#City').val(),
+                      Postcode: $('#Postcode').val(),   
+                      HouseNumber: $('#HouseNumber').val(),   
+                      Password: $('#Password').val(),   
+                      RepPassword: $('#RepPassword').val(),   
                 },
                 success: function (response)
                     {
                         
-                        if (response.match("Zalogowano"))
+                        if (response.match("Pomyślnie dodano rekord"))
                         {
-                          window.location.href = "/UserPanel.php";
+                          window.location.href = "/LogIn.php";
 
                         }
                         else{
-                              if (response.match("username")){
-                               $('#UserError').html('Nie poprawny login ):');
+                              if (response.match("1")){
+                               $('#UserError').html('Nie poprawny login.');
                               }else{
                                $('#UserError').html('');
                               }
-                              if (response.match("password")){
-                               $('#passwordError').html('Nie poprawne hasło D:');
+                            
+                              if (response.match("2")){
+                               $('#EmailError').html('Nie poprawny Email.');
                               }else{
-                               $('#passwordError').html('');
+                               $('#EmailError').html('');
+                                   }
+                            
+                              if (response.match("3")){
+                               $('#FirstNameError').html('Nie poprawne imie.');
+                              }else{
+                               $('#FirstNameError').html('');
+                                   }
+                            if (response.match("4")){
+                               $('#LastNameError').html('Nie poprawne nazwisko.');
+                              }else{
+                               $('#LastNameError').html('');
+                                   }
+                            
+                              if (response.match("5")){
+                               $('#CityError').html('Nie poprawne miasto.');
+                              }else{
+                               $('#CityError').html('');
+                                   }
+                            
+                              if (response.match("6")){
+                               $('#PostcodeError').html('Nie poprawny kod pocztowy');
+                              }else{
+                               $('#PostcodeError').html('');
+                                   }
+                            
+                              if (response.match("7")){
+                               $('#HouseNumberError').html('Nie poprawny numer domu.);
+                              }else{
+                               $('#HouseNumberError').html('');
+                                   }
+                            
+                              if (response.match("8")){
+                               $('#PasswordError').html('Nie poprawne hasło D:');
+                              }else{
+                               $('#PasswordError').html('');
+                                   }
+                            
+                              if (response.match("9")){
+                               $('#RepPasswordError').html('Hasła nie są takie same.');
+                              }else{
+                               $('#RepPasswordError').html('');
                               }
                            }
                         
