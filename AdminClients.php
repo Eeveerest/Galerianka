67b9bcc404a4bf76bef185db0a95314fe84311da
Eeveerest <moeve5699@gmail.com>
2024-11-28 10:01:07 +0100

    Update AdminClients.php
    Updated admin client site

--- AdminClients.php
+++ AdminClients.php
@@ -28,57 +28,16 @@ require_once 'database.php';
 
 <body>
   <div class="main">
-    <!--menu-->
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(23, 23, 23, 0.6)">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php" center>
-          <img
-            src="https://img.freepik.com/premium-zdjecie/kapibara-hydrochoerus-hydrochaeris-najwiekszy-zywy-gryzon-na-swiecie_45756-348.jpg?w=740"
-            alt="" width="70" height="54">Galerianka</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent1"
-          aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse text-center" id="navbarSupportedContent1">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="Fundations.php">Fundations</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link menu1 dropdown-toggle" id="Dropdown1" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                <b>Shop</b>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end" aria-labelledby="Dropdown1">
-                <li><a class="dropdown-item" href="Hunting.php">Hunting</a></li>
-                <li><a class="dropdown-item" href="Tactical.php">Tactical</a></li>
-                <li><a class="dropdown-item" href="BigKnives.php">Big Knives</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="navbar-brand menu-icon" href="LogIn.php">
-                <span><i class="fa-solid fa-user" alt="User"></i></span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="navbar-brand menu-icon" href="BasketPge.php">
-                <span><i class="fa-solid fa-basket-shopping" alt="Basket"></i></span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <!-- Header -->
+    <?php include('Header.php'); ?>
+    
     <!--Separator-->
     <nav class="navbar navbar-expand blue-border d-none d-md-block">
       <div class="container-fluid">
         <div class="collapse navbar-collapse" id="navbarSupportedContent2">
           <ul class="navbar-nav ms-auto">            
             <li class="nav-item">
-              <a class="nav-link active link-border" aria-current="page" href="AdminPanel.php">Back</a>
+              <a class="nav-link active link-border category-link" aria-current="page" href="AdminPanel.php">Back</a>
             </li>
           </ul>
       </div>
@@ -111,8 +70,6 @@ require_once 'database.php';
                         <th class="th1">Surname</th>
                         <th class="th1">Email</th>
                         
-                        <th></th>
-                        <th></th>
                         <th></th>
                     </tr>
                     </thead>
