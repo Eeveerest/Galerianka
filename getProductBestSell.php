c2cdad3c9d72ebb494c13f46aa4e0b378f3e79fc
Eeveerest <moeve5699@gmail.com>
2024-12-04 21:05:26 +0100

    Update getProductBestSell.php
    Fixed code generation

--- getProductBestSell.php
+++ getProductBestSell.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'database.php';
 
-$stmt = $pdo->query('SELECT p.id, name, price FROM `products`p INNER JOIN order_products op on op.product_id = p.id group by p.id order by count(op.product_id) desc limit 4');
+$stmt = $pdo->query('SELECT p.id, name, price FROM `products`p INNER JOIN order_products op on op.product_id = p.id group by p.id order by count(op.product_id) desc limit 8');
   
   
 if(isset($_SESSION['user_login'])){
@@ -19,17 +19,17 @@ $html = '';
   echo '<div id="BestsellerCarousel" class="carousel slide carousel1" data-bs-ride="BestsellerCarousel">';
   echo '<div class="carousel-inner">';    
   
-foreach ($stmt as $row){
-      if ($first == 0){
+  foreach ($stmt as $row){
+    $picture = $pdo->prepare('SELECT picture_one FROM `products_pictures` where product_id = '.$row['id'].'');
+    $picture ->execute();
+    $one = $picture ->fetch();
+    if ($first == 0){
       echo '<div class="carousel-item active">';
       $first = 1;
     }
     else if ($list == 1){
       echo '<div class="carousel-item">';     
     }
-    $picture = $pdo->prepare('SELECT picture_one FROM `products_pictures` where product_id = '.$row['id'].'');
-    $picture ->execute();
-    $one = $picture ->fetch();
     echo '<div class="col-md-6 col-lg-4 col-xl-3">';
     echo '<div class="thumb-wrapper">';
     echo '<div class="img-box picture-border">';
@@ -41,10 +41,10 @@ foreach ($stmt as $row){
     echo '<h4>'.$row['name'].'</h4></a>';
     echo '<p class="item-price"><b>'.$row['price'].' zł</b></p>';
     echo '<input type="submit" class="btn btn-success" name="Add_To" id="Add_To" onclick="Add_To('.$row['id'].')" value="Add to Cart" style="margin-right: 0.5rem">';
-     
-   
+    
+    
     if(isset($_SESSION['user_login'])){
-     echo '<span class="wish-icon" >';  
+      echo '<span class="wish-icon" >';  
       $i = 0;
       foreach ($result as $rows){       
         if ($row['id']==$rows['product'])
@@ -58,15 +58,16 @@ foreach ($stmt as $row){
       else {
         echo '<i class="fa fa-heart-o" id="heart_product_two_'.$row['id'].'" onclick="Favorite('.$row['id'].')"></i></span>';
       }
-      echo '</div></div></div>';
-    $list++;
-      if ($list >= 5){
-        echo '</div>';
-        $list = 1;
-      }
-      
     }
-}
+    echo '</div></div></div>';
+    $list++;
+    if ($list >= 5){
+      echo '</div>';
+      $list = 1;
+    }
+    
+  }
+
      echo '</div>';        
      echo '<a class="carousel-control-prev bg-transparent w-aut" href="#BestsellerCarousel" role="button" data-bs-slide="prev">';
      echo '<span class="carousel-control-prev-icon prev-icon1" aria-hidden="true"></span></a>';         
